42e337f5b0b305d1caffd614d3be2ad2fef20634
UndertakerBen <60009857+UndertakerBen@users.noreply.github.com>
2020-10-07 10:27:16 +0200

    Added Launcher Sourcecode

---
+++ Launcher Sourcecode/Math Launcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace Portable_Libre_Math_Launcher
+{
+    class Program
+    {
+        static void Main()
+        {
+            string applicationPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            var sb = new System.Text.StringBuilder();
+            string[] CommandLineArgs = Environment.GetCommandLineArgs();
+            for (int i = 1; i < CommandLineArgs.Length; i++)
+            {
+                sb.Append(" \"" + CommandLineArgs[i] + "\"");
+            }
+            Process.Start(applicationPath + "\\Libre Office\\program\\smath.exe", sb.ToString());
+        }
+    }
+}
---
+++ Launcher Sourcecode/Office Launcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace Portable_Libre_Office_Launcher
+{
+    class Program
+    {
+        static void Main()
+        {
+            string applicationPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            var sb = new System.Text.StringBuilder();
+            string[] CommandLineArgs = Environment.GetCommandLineArgs();
+            for (int i = 1; i < CommandLineArgs.Length; i++)
+            {
+                sb.Append(" \"" + CommandLineArgs[i] + "\"");
+            }
+            Process.Start(applicationPath + "\\Libre Office\\program\\soffice.exe", sb.ToString());
+        }
+    }
+}
